a46dcfc470b8d4c001e903d2af682af65ad0f4a9
Luke Walker4 <lwalker128005@gmail.com>
2018-04-24 11:23:25 -0600

    changes

--- FinalProject/Form1.Designer.cs
+++ FinalProject/Form1.Designer.cs
@@ -28,13 +28,278 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtGenBox0 = new System.Windows.Forms.TextBox();
+            this.txtGenBox1 = new System.Windows.Forms.TextBox();
+            this.txtGenBox2 = new System.Windows.Forms.TextBox();
+            this.txtGenBox3 = new System.Windows.Forms.TextBox();
+            this.txtGenBox4 = new System.Windows.Forms.TextBox();
+            this.txtGenBox5 = new System.Windows.Forms.TextBox();
+            this.txtGenBox6 = new System.Windows.Forms.TextBox();
+            this.btnGenStory = new System.Windows.Forms.Button();
+            this.btnSaveStory = new System.Windows.Forms.Button();
+            this.txtParam1 = new System.Windows.Forms.TextBox();
+            this.txtParam2 = new System.Windows.Forms.TextBox();
+            this.txtParm3 = new System.Windows.Forms.TextBox();
+            this.txtParm4 = new System.Windows.Forms.TextBox();
+            this.txtParm5 = new System.Windows.Forms.TextBox();
+            this.txtParm6 = new System.Windows.Forms.TextBox();
+            this.txtParm7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtGenBox0
+            // 
+            this.txtGenBox0.Location = new System.Drawing.Point(595, 49);
+            this.txtGenBox0.Multiline = true;
+            this.txtGenBox0.Name = "txtGenBox0";
+            this.txtGenBox0.Size = new System.Drawing.Size(227, 80);
+            this.txtGenBox0.TabIndex = 0;
+            // 
+            // txtGenBox1
+            // 
+            this.txtGenBox1.Location = new System.Drawing.Point(475, 159);
+            this.txtGenBox1.Multiline = true;
+            this.txtGenBox1.Name = "txtGenBox1";
+            this.txtGenBox1.Size = new System.Drawing.Size(227, 80);
+            this.txtGenBox1.TabIndex = 1;
+            // 
+            // txtGenBox2
+            // 
+            this.txtGenBox2.Location = new System.Drawing.Point(727, 159);
+            this.txtGenBox2.Multiline = true;
+            this.txtGenBox2.Name = "txtGenBox2";
+            this.txtGenBox2.Size = new System.Drawing.Size(227, 80);
+            this.txtGenBox2.TabIndex = 2;
+            // 
+            // txtGenBox3
+            // 
+            this.txtGenBox3.Location = new System.Drawing.Point(595, 265);
+            this.txtGenBox3.Multiline = true;
+            this.txtGenBox3.Name = "txtGenBox3";
+            this.txtGenBox3.Size = new System.Drawing.Size(227, 80);
+            this.txtGenBox3.TabIndex = 3;
+            // 
+            // txtGenBox4
+            // 
+            this.txtGenBox4.Location = new System.Drawing.Point(475, 373);
+            this.txtGenBox4.Multiline = true;
+            this.txtGenBox4.Name = "txtGenBox4";
+            this.txtGenBox4.Size = new System.Drawing.Size(227, 80);
+            this.txtGenBox4.TabIndex = 4;
+            // 
+            // txtGenBox5
+            // 
+            this.txtGenBox5.Location = new System.Drawing.Point(727, 373);
+            this.txtGenBox5.Multiline = true;
+            this.txtGenBox5.Name = "txtGenBox5";
+            this.txtGenBox5.Size = new System.Drawing.Size(227, 80);
+            this.txtGenBox5.TabIndex = 5;
+            // 
+            // txtGenBox6
+            // 
+            this.txtGenBox6.Location = new System.Drawing.Point(595, 483);
+            this.txtGenBox6.Multiline = true;
+            this.txtGenBox6.Name = "txtGenBox6";
+            this.txtGenBox6.Size = new System.Drawing.Size(227, 80);
+            this.txtGenBox6.TabIndex = 6;
+            // 
+            // btnGenStory
+            // 
+            this.btnGenStory.Location = new System.Drawing.Point(44, 515);
+            this.btnGenStory.Name = "btnGenStory";
+            this.btnGenStory.Size = new System.Drawing.Size(75, 47);
+            this.btnGenStory.TabIndex = 7;
+            this.btnGenStory.Text = "Generate Story";
+            this.btnGenStory.UseVisualStyleBackColor = true;
+            this.btnGenStory.Click += new System.EventHandler(this.btnGenStory_Click);
+            // 
+            // btnSaveStory
+            // 
+            this.btnSaveStory.Location = new System.Drawing.Point(169, 516);
+            this.btnSaveStory.Name = "btnSaveStory";
+            this.btnSaveStory.Size = new System.Drawing.Size(75, 47);
+            this.btnSaveStory.TabIndex = 8;
+            this.btnSaveStory.Text = "Save Story";
+            this.btnSaveStory.UseVisualStyleBackColor = true;
+            // 
+            // txtParam1
+            // 
+            this.txtParam1.Location = new System.Drawing.Point(125, 74);
+            this.txtParam1.Name = "txtParam1";
+            this.txtParam1.Size = new System.Drawing.Size(126, 20);
+            this.txtParam1.TabIndex = 9;
+            // 
+            // txtParam2
+            // 
+            this.txtParam2.Location = new System.Drawing.Point(125, 132);
+            this.txtParam2.Name = "txtParam2";
+            this.txtParam2.Size = new System.Drawing.Size(126, 20);
+            this.txtParam2.TabIndex = 10;
+            // 
+            // txtParm3
+            // 
+            this.txtParm3.Location = new System.Drawing.Point(125, 195);
+            this.txtParm3.Name = "txtParm3";
+            this.txtParm3.Size = new System.Drawing.Size(126, 20);
+            this.txtParm3.TabIndex = 11;
+            // 
+            // txtParm4
+            // 
+            this.txtParm4.Location = new System.Drawing.Point(125, 249);
+            this.txtParm4.Name = "txtParm4";
+            this.txtParm4.Size = new System.Drawing.Size(126, 20);
+            this.txtParm4.TabIndex = 12;
+            // 
+            // txtParm5
+            // 
+            this.txtParm5.Location = new System.Drawing.Point(125, 304);
+            this.txtParm5.Name = "txtParm5";
+            this.txtParm5.Size = new System.Drawing.Size(126, 20);
+            this.txtParm5.TabIndex = 13;
+            // 
+            // txtParm6
+            // 
+            this.txtParm6.Location = new System.Drawing.Point(125, 356);
+            this.txtParm6.Name = "txtParm6";
+            this.txtParm6.Size = new System.Drawing.Size(126, 20);
+            this.txtParm6.TabIndex = 14;
+            // 
+            // txtParm7
+            // 
+            this.txtParm7.Location = new System.Drawing.Point(125, 411);
+            this.txtParm7.Name = "txtParm7";
+            this.txtParm7.Size = new System.Drawing.Size(126, 20);
+            this.txtParm7.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Parameter 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Parameter 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Parameter 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Parameter 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 311);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Parameter 5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Parameter 6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(26, 418);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Parameter 7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1183, 618);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtParm7);
+            this.Controls.Add(this.txtParm6);
+            this.Controls.Add(this.txtParm5);
+            this.Controls.Add(this.txtParm4);
+            this.Controls.Add(this.txtParm3);
+            this.Controls.Add(this.txtParam2);
+            this.Controls.Add(this.txtParam1);
+            this.Controls.Add(this.btnSaveStory);
+            this.Controls.Add(this.btnGenStory);
+            this.Controls.Add(this.txtGenBox6);
+            this.Controls.Add(this.txtGenBox5);
+            this.Controls.Add(this.txtGenBox4);
+            this.Controls.Add(this.txtGenBox3);
+            this.Controls.Add(this.txtGenBox2);
+            this.Controls.Add(this.txtGenBox1);
+            this.Controls.Add(this.txtGenBox0);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtGenBox0;
+        private System.Windows.Forms.TextBox txtGenBox1;
+        private System.Windows.Forms.TextBox txtGenBox2;
+        private System.Windows.Forms.TextBox txtGenBox3;
+        private System.Windows.Forms.TextBox txtGenBox4;
+        private System.Windows.Forms.TextBox txtGenBox5;
+        private System.Windows.Forms.TextBox txtGenBox6;
+        private System.Windows.Forms.Button btnGenStory;
+        private System.Windows.Forms.Button btnSaveStory;
+        private System.Windows.Forms.TextBox txtParam1;
+        private System.Windows.Forms.TextBox txtParam2;
+        private System.Windows.Forms.TextBox txtParm3;
+        private System.Windows.Forms.TextBox txtParm4;
+        private System.Windows.Forms.TextBox txtParm5;
+        private System.Windows.Forms.TextBox txtParm6;
+        private System.Windows.Forms.TextBox txtParm7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- FinalProject/Form1.cs
+++ FinalProject/Form1.cs
@@ -12,9 +12,39 @@ namespace FinalProject
 {
     public partial class Form1 : Form
     {
+        static List<List<string>> StoryParts = new List<List<string>>();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnGenStory_Click(object sender, EventArgs e)
+        {
+            List<string> tempList = new List<string>();
+            CreateBox0(tempList);
+            CreateBox1(tempList);
+            CreateBox2(tempList);
+            CreateBox3(tempList);
+            CreateBox4(tempList);
+            CreateBox5(tempList);
+
+            StoryParts.Add(tempList);
+        }
+
+        private void CreationBox0(List<string> sectionList)
+        {
+            string tempString = "Jim Bob";
+            txtGenBox0.Text = tempString;
+            sectionList.Add(tempString);
+        }
+
+        private void CreationBox1(List<string> sectionList)
+        {
+            string tempString = "like to";
+            txtGenBox1.Text = tempString;
+            sectionList.Add(tempString);
+        }
+
     }
 }
